a0c8c946fd2ac0b4f26885ae60cb9446cae12688
NewL <taemin1104@naver.com>
2022-11-17 13:50:16 +0900

    feat: Link 적용

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import "./App.css";
 import MainPageComponent from "./main/index.js";
+import { Switch, Route } from "react-router-dom";
+import UploadPage from "./upload";
+import ProductPage from "./product";
 
 function App() {
-  return <MainPageComponent></MainPageComponent>;
+  return (
+    <div>
+      <Switch>
+        <Route exact={true} path="/">
+          <MainPageComponent></MainPageComponent>
+        </Route>
+        <Route exact={true} path="/products/:id">
+          <ProductPage></ProductPage>
+        </Route>
+        <Route exact={true} path="/upload">
+          <UploadPage></UploadPage>
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
